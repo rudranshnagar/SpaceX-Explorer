8021ea9f7fa94bd65dc0538aa74a884c5b6b070b
rudranshnagar <rnagar1@stvens.edu>
2024-03-22 03:31:16 -0400

    search

--- src/components/Common/searchres.jsx
+++ src/components/Common/searchres.jsx
@@ -1,29 +1,83 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-
-import React from 'react';
+import RocketsTable from "../Rockets/RocketsTable";
+import PayloadsTable from "../Payloads/PayloadsTable";
+import CoresTable from "../Cores/CoresTable";
+import React from "react";
 
 const Searchres = () => {
-    const { searchTerm } = useParams();
-    const [searchdata, setsearchdata] = useState([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
+  const { searchTerm } = useParams();
+  const [searchData, setSearchData] = useState({});
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const requestBody = {
+          query: {
+            $text: {
+              $search: searchTerm.trim(),
+            },
+          },
+        };
+        const rockets = await axios.post(
+          `https://api.spacexdata.com/v4/rockets/query`,
+          requestBody
+        );
+        const payloads = await axios.post(
+          `https://api.spacexdata.com/v4/payloads/query`,
+          requestBody
+        );
+        const cores = await axios.post(
+          `https://api.spacexdata.com/v4/cores/query`,
+          requestBody
+        );
+
+        searchData.rockets = rockets.data.docs;
+        searchData.payloads = payloads.data.docs;
+        searchData.cores = cores.data.docs;
+
+        setSearchData(searchData);
+
         setLoading(false);
-    },[]);
-    if (loading) {
-        return (
-          <div>
-            <h1>Loading.....</h1>
-          </div>
-        );
-      } else {
-        return (
-          <div className="searchres">
-            <h1>Search Results for "{searchTerm}"</h1>
-          </div>
-        );
+      } catch (error) {
+        console.log(error);
       }
-}
+    }
+    fetchData();
+  }, [searchTerm]);
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading.....</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div className="searchres">
+        <h1>Search Results for "{searchTerm}"</h1>
+        <h1>Rockets result:</h1>
+        {searchData.rockets.length > 0 ? (
+          <RocketsTable rocketsData={searchData.rockets} />
+        ) : (
+          <h4>"No data found"</h4>
+        )}
+        <h1>Payloads result:</h1>
+        {searchData.payloads.length > 0 ? (
+          <PayloadsTable payloadsData={searchData.payloads} />
+        ) : (
+          <h4>"No data found"</h4>
+        )}
+        <h1>Cores result:</h1>
+        {searchData.cores.length > 0 ? (
+          <CoresTable coresData={searchData.cores} />
+        ) : (
+          <h4>"No data found"</h4>
+        )}
+      </div>
+    );
+  }
+};
 
 export default Searchres;
