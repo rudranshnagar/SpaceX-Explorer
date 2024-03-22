c65ed760dfde1fd30334662ee6040e88d88be548
yash9323 <bhutoriayash1999@gmail.com>
2024-03-22 01:55:28 -0400

    footer

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -5,7 +5,6 @@ import CopyrightOutlinedIcon from "@mui/icons-material/CopyrightOutlined";
 import LanguageIcon from "@mui/icons-material/Language";
 import CameraIcon from "@mui/icons-material/Camera";
 import XIcon from "@mui/icons-material/X";
-import PlaceIcon from "@mui/icons-material/Place";
 
 const Company = () => {
   const [companyData, setCompanyData] = useState([]);
@@ -34,64 +33,38 @@ const Company = () => {
   } else {
     return (
       <div className="footer">
-        <div className="footer-left">
-          <div className="footer-title">
-            <Typography variant="h5" align="center" gutterBottom>
-              <div className="cc-symbol">
-                <CopyrightOutlinedIcon />
-                {companyData.name}
-              </div>
-            </Typography>
-          </div>
-          <div className="footer-summary">
-            <Typography
-              variant="subtitle1"
-              align="center"
-              color="text.secondary"
-              component="p"
-            >
-              {companyData.summary}
-            </Typography>
-          </div>
+        <div className="logg">
+        <h1>{companyData.name}</h1>
+        <CopyrightOutlinedIcon /> 
         </div>
-        <div className="footer-center">
-          <div className="footer-website-icon">
+        <div className="add">
+        <h3>{companyData.headquarters.address}, {companyData.headquarters.city}, {companyData.headquarters.state}</h3>
+        </div>
+        <div className="summ">
+          {companyData.summary}
+        </div>
+        <br />
+        <div className="social-links-dev">
+        <div className="footer-twitter-icon">
+            <a href={companyData.links.twitter}>
+              <XIcon />
+            </a>
+          </div>
+        <div className="footer-website-icon">
             <a href={companyData.links.website}>
               <LanguageIcon />
-              Official Website
-            </a>
-          </div>
-          <div className="footer-flickr-icon">
-            <a href={companyData.links.flickr}>
-              <CameraIcon />
-              Flickr
-            </a>
-          </div>
-          <div className="footer-twitter-icon">
-            <a href={companyData.links.twitter}>
-              <XIcon />X
             </a>
           </div>
           <div className="footer-ceo-twitter-icon">
             <a href={companyData.links.twitter}>
               <XIcon />
-              Elon Musk
             </a>
           </div>
-        </div>
-        <div className="footer-right">
-          <Typography
-            fontWeight="bold"
-            variant="h5"
-            align="center"
-            gutterBottom
-          >
-            <div className="footer-place-icon">
-              <PlaceIcon />
-            </div>
-            {companyData.headquarters.address} ,{companyData.headquarters.city}{" "}
-            ,{companyData.headquarters.state}
-          </Typography>
+          <div className="footer-flickr-icon">
+            <a href={companyData.links.flickr}>
+              <CameraIcon />
+            </a>
+          </div>
         </div>
       </div>
     );
